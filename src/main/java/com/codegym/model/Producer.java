70c4bf9b7b544b7a92e59fd01aef45c3f5cc8d33
huy8895 <huy8895@gmail.com>
2020-09-29 14:50:12 +0700

    tao thanh cong 1 phone bang ajax

--- src/main/java/com/codegym/model/Producer.java
+++ src/main/java/com/codegym/model/Producer.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Table(name = "producers")
 public class Producer {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
--- src/main/java/com/codegym/model/Smartphone.java
+++ src/main/java/com/codegym/model/Smartphone.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Smartphone {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
